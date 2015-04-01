2fead285fb7b57f7021b728f8acec87e723e6920
Anh <anhpnguyen.an@gmail.com>
2015-04-01 10:55:57 -0400

    change timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+Time.zone = "Eastern Time (US & Canada)"
+Time.zone.name
+Time.zone.now
